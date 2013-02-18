daa9379fc1a902a2324af09df1e289732b583840
Hippie Hacker <chris@hippiehacker.org>
2013-02-18 00:29:18 -0800

    moving chef client files to data bag search

--- recipes/default.rb
+++ recipes/default.rb
@@ -30,32 +30,32 @@ directory node['fileserver']['docroot'] do
   recursive true
 end
 
-directory File.join(node['fileserver']['docroot'],'slides') do
-  owner node['apache']['user']
-  mode 00755
-end
+# directory File.join(node['fileserver']['docroot'],'slides') do
+#   owner node['apache']['user']
+#   mode 00755
+# end
 
-targets_listing_dir = File.join(node['fileserver']['docroot'],'targets')
-directory targets_listing_dir do
-  owner node['apache']['user']
-  mode 00755
-end
+# targets_listing_dir = File.join(node['fileserver']['docroot'],'targets')
+# directory targets_listing_dir do
+#   owner node['apache']['user']
+#   mode 00755
+# end
 
-target_list = []
-Dir.glob('/var/lib/lxc/target*').each do |target_dir| 
-  target_name = File.basename(target_dir)
-  target_list << target_name
-  link File.join(targets_listing_dir,target_name) do
-    to File.join(target_dir,'rootfs')
-    owner node['apache']['user']
-  end
-end
+# target_list = []
+# Dir.glob('/var/lib/lxc/target*').each do |target_dir| 
+#   target_name = File.basename(target_dir)
+#   target_list << target_name
+#   link File.join(targets_listing_dir,target_name) do
+#     to File.join(target_dir,'rootfs')
+#     owner node['apache']['user']
+#   end
+# end
 
-workstation_list = []
-Dir.glob('/var/lib/lxc/workstation*').each do |workstation_dir| 
-  workstation_name = File.basename(workstation_dir)
-  workstation_list << workstation_name
-end
+# workstation_list = []
+# Dir.glob('/var/lib/lxc/workstation*').each do |workstation_dir| 
+#   workstation_name = File.basename(workstation_dir)
+#   workstation_list << workstation_name
+# end
 
 index_data << "<br /><a href=\"chef-repo.zip\">Chef Repository zip file</a>"
 
@@ -64,8 +64,8 @@ template File.join(node['fileserver']['docroot'], "index.html") do
   mode 00644
   variables({
       :index_data => index_data,
-      :workstation_list => workstation_list,
-      :target_list => target_list
+      #:workstation_list => workstation_list,
+      #:target_list => target_list
     })
 end
 
@@ -82,45 +82,27 @@ node['fileserver']['sublime'].each do |os, data|
 
 end
 
-node['fileserver']['chef_full']['platforms'].each do |os, data|
-
-  v = node['fileserver']['chef_full']['version']
-  p = data['platform']
-  pv = data['platform_version']
-  m = data['machine']
-
-  ext = data['platform'] =~ /ubuntu/ ? "deb" : "rpm"
-  if data.has_key?("url")
-    url = data['url']
-  else
-    url = "https://www.opscode.com/chef/download?v=#{v}&p=#{p}&pv=#{pv}&m=#{m}"
-  end
-
-  if data.has_key?("filename")
-    filename = data['filename']
-  else
-    filename = "chef-full-#{v}.#{m}.#{ext}"
-  end
-
-  index_data << "<br /><a href=\"#{filename}\">Chef Full Stack for #{os.capitalize}</a>"
-
-  remote_file File.join(node['fileserver']['docroot'], filename) do
-    source url
-    checksum data['checksum']
+search('chef',"version:#{node['chef_client']['version']}").each do |c|
+  oses = c['os'].map do |os,versions|
+    "#{os} #{versions.join(', ')}"
+  end.join('; ')
+  index_data << "<br /><a href=\"#{c['filename']}\">Chef Full Stack for #{oses}</a>"
+  remote_file File.join(node['fileserver']['docroot'], c['filename']) do
+    source c['source']
+    checksum c['checksum']
     owner node['apache']['user']
     mode 00644
   end
-
 end
 
-# index_data << "<br /><a href=\"cotvnc.dmg\">VNC Client for OSX</a>"
+index_data << "<br /><a href=\"cotvnc.dmg\">VNC Client for OSX</a>"
 
-# remote_file File.join(node['fileserver']['docroot'], "cotvnc.dmg") do
-#   source "http://hivelocity.dl.sourceforge.net/project/chicken/Chicken-2.2b2.dmg"
-#   checksum "20e910b6cbf95c3e5dcf6fe8e120d5a0911f19099128981fb95119cee8d5fc6b"
-#   owner node['apache']['user']
-#   mode 00644
-# end
+remote_file File.join(node['fileserver']['docroot'], "cotvnc.dmg") do
+  source "http://hivelocity.dl.sourceforge.net/project/chicken/Chicken-2.2b2.dmg"
+  checksum "20e910b6cbf95c3e5dcf6fe8e120d5a0911f19099128981fb95119cee8d5fc6b"
+  owner node['apache']['user']
+  mode 00644
+end
 
 index_data << "<br /><a href=\"tightvnc.msi\">VNC Client for Windows</a>"
 
@@ -131,36 +113,9 @@ remote_file File.join(node['fileserver']['docroot'], "tightvnc.msi") do
   mode 00644
 end
 
-index_data << "<br /><a href=\"Anatomy-of-Chef-Run-AuthN.pdf\">Anatomy of a Chef Run (PDF)</a>"
-
-remote_file File.join(node['fileserver']['docroot'], "Anatomy-of-Chef-Run-AuthN.pdf") do
-  source "https://opscode-chef-training.s3.amazonaws.com/Anatomy-of-Chef-Run-AuthN.pdf"
-  checksum "7fc9c66d64d8aec02014dc930e4436cb6d945d24c75944d1efaf4e9edc623d3b"
-  owner node['apache']['user']
-  mode 00644
-end
-
-index_data << "<br /><a href=\"Chef-Workshop-CheatSheet.pdf\">Chef Workshop Cheat Sheet (PDF)</a>"
-
-remote_file File.join(node['fileserver']['docroot'], "Chef-Workshop-CheatSheet.pdf") do
-  source "https://opscode-chef-training.s3.amazonaws.com/Chef-Workshop-CheatSheet.pdf"
-  checksum "4328cc2073a3a4133cb82649b6903729a8d3947bb1f866dc62845eeb986fa9f0"
-  owner node['apache']['user']
-  mode 00644
-end
-
-index_data << "<br /><a href=\"Chef101-Terminology.pdf\">Chef 101 Terminology (PDF)</a>"
-
-remote_file File.join(node['fileserver']['docroot'], "Chef101-Terminology.pdf") do
-  source "https://opscode-chef-training.s3.amazonaws.com/Chef101-Terminology.pdf"
-  checksum "e396fffda73e19bd7ff7ad3affc1e8c0ea9b697a8f3d1ed19b090c0930f389f6"
-  owner node['apache']['user']
-  mode 00644
-end
-
-link "chef-full.deb" do
-  to node['fileserver']['chef_full']['debian_32']['filename']
-end if node['fileserver']['chef_full'].has_key?('debian_32')
+# link "chef-full.deb" do
+#   to node['fileserver']['chef_full']['debian_32']['filename']
+# end if node['fileserver']['chef_full'].has_key?('debian_32')
 
 cookbook_file File.join(node['fileserver']['docroot'], "chef-repo.zip") do
   source "chef-repo.zip"
